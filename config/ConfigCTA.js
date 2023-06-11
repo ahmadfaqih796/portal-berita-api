780d1fcc4e9688d21d506852deb1a0703160351c
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-11 13:19:20 +0700

    config CTA response #2

---
+++ config/ConfigCTA.js
@@ -0,0 +1,32 @@
+const ConfigCTA = {};
+
+ConfigCTA.CTA_MESSAGE_REQUEST_ERROR = {
+  title: "Sorry",
+  message:
+    "Terjadi kesalahan saat memproses data. Silahkan periksa kembali informasi yang Anda masukkan dan pastikan semuanya diisi dengan benar",
+};
+
+ConfigCTA.CTA_MESSAGE_SIGNIN_ERROR = {
+  title: "Sorry",
+  message:
+    "Terjadi kesalahan saat masuk. Periksa kembali email dan kata sandi Anda, pastikan semuanya benar.",
+};
+
+ConfigCTA.CTA_MESSAGE_SUCCESS_SIGNUP = {
+  title: "Sukses!",
+  message: "Anda berhasil melakukan pendaftaran. Silahkan sign in untuk masuk.",
+};
+
+ConfigCTA.CTA_MESSAGE_TOKEN_ERROR = {
+  title: "Sorry",
+  message:
+    "Anda perlu menyertakan token untuk proses otentikasi atau Anda bisa melakukan sign in ulang.",
+};
+
+ConfigCTA.CTA_MESSAGE_TOKEN_INVALID = {
+  title: "Sorry",
+  message:
+    "Mohon maaf, sesi login Anda telah berakhir. Silakan signin kembali.",
+};
+
+module.exports = ConfigCTA;
