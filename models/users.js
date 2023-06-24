5521269259032e76b0ab9be00ca4dc64a675dc79
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-24 22:46:52 +0700

    router

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const User = sequelize.define("User", {
+const User = sequelize.define("users", {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/users");
 
 // Register route
 router.post("/register", (req, res) => {
