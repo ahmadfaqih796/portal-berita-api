f250172431d27db8e7e771b0637b7bd0635d2c43
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-25 21:53:40 +0700

    auth middleware #9

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,12 @@
-// middlewares/authMiddleware.js
-
 const jwt = require("jsonwebtoken");
-
-const jwtSecret =
-  "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NzcwMTI4OSwiaWF0IjoxNjg3NzAxMjg5fQ.WIPnvWSEYmUOyFzYTt0P92dOoVFQZaryU-0m6g1CeXk"; // Ganti dengan secret key yang lebih aman
+const { SECRET_KEY } = process.env;
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, jwtSecret, (err, user) => {
+  jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
