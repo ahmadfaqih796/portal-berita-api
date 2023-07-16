2885d84afb178f03ad4a8b51260bb53daf4d1a90
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-16 21:25:32 +0700

    article route

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,1 +1,24 @@
 const { Article } = require("../models");
+
+const getAllArticle = async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const offset = (page - 1) * limit;
+    const total = await Article.count();
+    const users = await Article.findAll({
+      offset,
+      limit: parseInt(limit),
+    });
+    res.status(200).json({
+      total,
+      limit: parseInt(limit),
+      skip: offset,
+      data: users,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { getAllArticle };
---
+++ routes/article.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const articleController = require("../controllers/articleController");
+const { authenticateToken } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Menambahkan middleware authenticateToken untuk memeriksa token pada semua rute user
+router.use(authenticateToken);
+
+// Rute untuk mendapatkan semua pengguna
+router.get("/", articleController.getAllArticle);
+
+module.exports = router;
