36ebfed3ec180b21acb7a1860f6203ed9c8a3d99
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-24 09:12:23 +0700

    router article

--- routes/article.js
+++ routes/article.js
@@ -4,11 +4,12 @@ const { authenticateToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Menambahkan middleware authenticateToken untuk memeriksa token pada semua rute user
 router.use(authenticateToken);
 
-// Rute untuk mendapatkan semua pengguna
 router.get("/", articleController.getAllArticle);
+router.get("/:id", articleController.getArticleById);
 router.post("/", articleController.createArticle);
+router.patch("/:id", articleController.updateArticleById);
+router.delete("/:id", articleController.deleteArticleById);
 
 module.exports = router;
